49e579123030c8705691313f66558e8d1f3677d7
Ankit Kumar <imakki1410@gmail.com>
2024-05-07 02:44:00 +0530

    Finished

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f51a2b0726msh2c844353b12df1ep13a436jsn445ce0dff747',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL  = 'https://api.openweathermap.org/data/2.5';  
+
+export const WEATHER_API_KEY = "407cb2c23075b36d1b83f5cf055d72de";
